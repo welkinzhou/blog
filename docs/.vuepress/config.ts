7e9523eb7295c9ea273791ee1440ffd5052e8311
welkin <welkin105380@163.com>
2023-07-11 16:17:46 +0800

    init: 创建项目基本结构

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,8 @@
+import { defineUserConfig } from 'vuepress'
+
+export default defineUserConfig({
+    lang: 'zh-CN',
+    title: '守拙',
+    description: 'welkin 的互联网小窝',
+    head: [['link', { rel: 'icon', href: '/images/logo.jpg' }]],
+})
