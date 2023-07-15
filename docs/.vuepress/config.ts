674039f7ef3acfab339f4a95e74054760f9d6e95
welkin <welkin105380@163.com>
2023-07-15 13:50:12 +0800

    chore: 添加基本结构，GitHub Actions

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,25 @@
-import { defineUserConfig } from 'vuepress'
+import { defineUserConfig, defaultTheme } from 'vuepress'
+import { nprogressPlugin } from '@vuepress/plugin-nprogress'
 
 export default defineUserConfig({
     lang: 'zh-CN',
     title: '守拙',
     description: 'welkin 的互联网小窝',
     head: [['link', { rel: 'icon', href: '/images/logo.jpg' }]],
+    theme: defaultTheme({
+        colorModeSwitch: true,
+        repo: 'https://github.com/welkinzhou/blog',
+        logo: '/images/logo.jpg',
+        // 默认主题配置
+        navbar: [
+            {
+                text: '首页',
+                link: '/',
+            },
+        ],
+        sidebar: 'auto',
+    }),
+    plugins: [
+        nprogressPlugin(),
+    ],
 })
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,12 @@
-# Hello VuePress
+---
+home: true
+lang: zh-CN
+title: 开始的地方
+description: 记录生活，记录工作，水过留痕
+---
+
+## Hello VuePress
+
+## 测试
+
+[css 学习笔记](./articles/css.md)
